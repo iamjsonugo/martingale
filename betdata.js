e9bb591c4f4e4823bc6de4048323f3300b38495d
iamjsonugo <86761845+iamjsonugo@users.noreply.github.com>
2022-05-13 05:40:46 +1200

    Update betdata.js

--- betdata.js
+++ betdata.js
@@ -1,5 +1,3 @@
-function sixOddReport() {}
-
 function betData(search) {
   let text = document.querySelector(".bet-data-textarea").value;
 
@@ -71,48 +69,47 @@ function betData(search) {
             alert("Bet Data Saved!");
           });
         */
-  function processData() {
-    let reports = {};
-    reports.winData = [];
+  let reports = {};
 
-    let currentWeek = 0;
+  //process winData
+  reports.winData = [];
+  for (let i = 0; i < season.length; i++) {
+    for (let j = 0; j < season[i].length; j++) {
+      const match = season[i][j];
+      let score = match.replace(/#/gi, "");
+      let reportScore = score;
+      score = score.split("-");
+      let teamAName = score[0].replace(/\d/gi, "");
+      let teamBName = score[1].replace(/\d/gi, "");
+      let teamA = score[0].replace(/\D/gi, "");
+      let teamB = score[1].replace(/\D/gi, "");
+      let position = j + 1;
+      let week = i + 1;
 
-    for (let i = 0; i < season.length; i++) {
-      for (let j = 0; j < season[i].length; j++) {
-        const match = season[i][j];
-        let score = match.replace(/#/gi, "");
-        let reportScore = score;
-        score = score.split("-");
-        let teamAName = score[0].replace(/\d/gi, "");
-        let teamBName = score[1].replace(/\d/gi, "");
-        let teamA = score[0].replace(/\D/gi, "");
-        let teamB = score[1].replace(/\D/gi, "");
-        let position = j + 1;
-        let week = i + 1;
-
-        //Correct Score
-        if (`${teamA}-${teamB}` === "2-1") {
-          reports.winData.push(`WK${week}: ${position} ${reportScore}`);
-        } else {
-          console.log("No Correct Score found");
-        }
+      //Correct Score
+      if (`${teamA}-${teamB}` === "2-1") {
+        reports.winData.push(`WK${week}: ${position} ${reportScore}`);
       }
     }
-
-    //position
-    for (let i = 0; i < reports.winData.length; i++) {    
-      const match = reports.winData[i];
-      let matchWeek = match.split(":")[0];
-      let matchOutcome = match.split(":")[1]
-      matchWeek = parseInt(matchWeek.replace(/WK/, ""));
-      matchOutcome = parseInt(matchOutcome.replace(/\w\w\w/, ""));
-      if (currentWeek == matchWeek){continue}
-      currentWeek = matchWeek;
-      console.log(matchWeek);
-    }
-    console.table(reports.match)
   }
-  processData();
+
+  //process position
+  let currentWeek = 0;
+  let position = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  for (let i = 0; i < reports.winData.length; i++) {
+    const match = reports.winData[i];
+    let matchWeek = match.split(":")[0];
+    let matchOutcome = match.split(":")[1];
+    matchWeek = parseInt(matchWeek.replace(/WK/, ""));
+    matchOutcome = parseInt(matchOutcome.replace(/ /, " "));
+    if (matchOutcome === 1) {
+      if (currentWeek === matchWeek) {
+        continue;
+      }
+      currentWeek = matchWeek;
+      console.log(matchWeek + "&" + matchOutcome);
+    }
+  }
 }
 
 document.querySelector(".bet-data-btn").addEventListener("click", (event) => {
