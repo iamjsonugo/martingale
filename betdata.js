9c882e12191c6f709141a0a5f383e79550aecfae
Johnson Ugo <86761845+iamjsonugo@users.noreply.github.com>
2022-05-28 03:21:33 +0100

    Update betdata.js

--- betdata.js
+++ betdata.js
@@ -274,5 +274,5 @@ async function paste(input) {
 document.querySelector(".modal-body").innerHTML = "";
 const keys = Object.keys(localStorage);
 for (let key of keys) {
-   document.querySelector(".modal-body").innerHTML += `${key}: <button class="btn btn-sm" style="background-color:lightgrey;" onclick="navigator.clipboard.writeText(${localStorage.getItem(key)});this.style.background='orange';">Copy</button><br> ${localStorage.getItem(key)} <hr>`;
+   document.querySelector(".modal-body").innerHTML += `${key}: <button class="btn btn-sm" style="background-color:lightgrey;" onclick="navigator.clipboard.writeText(${localStorage.getItem(key)});this.style.background='orange';">Copy</button><br> ${JSON.parse(localStorage.getItem(key).data)} <hr>`;
 }
